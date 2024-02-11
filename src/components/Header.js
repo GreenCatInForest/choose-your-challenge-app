86a65828db2aa38d96c13eb4ecf7082d41fb13be
GreenCatInForest <event2016.2017@gmail.com>
2024-02-11 18:31:39 +0000

    style mobile header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,24 @@ import jsLogo from "../assets/images/js-logo.png";
 export const Header = () => {
   return (
     <div className="flex flex-col justify-center text-center items-center">
-      <div className="flex justify-center text-center items-center">
+      <div className="flex flex-col sm:flex-row justify-center text-center items-center">
         <a href="/">
-          <h1 className="text-5xl sm:text-3xl font-bold mr-12 hover:transform hover:scale-150">
+          <h1 className="text-5xl sm:text-3xl font-bold md:mr-12 hover:transform hover:scale-150">
             YK
           </h1>
         </a>
-        <div className="title flex flex-row items-center">
+        <div className="title flex flex-col sm:flex-row items-center">
           <h2>Find Your</h2>
           <img src={jsLogo} alt="Logo JS" className="w-14" />
           <h1>Challenge</h1>
         </div>
       </div>
-      <h2>1. Practice your JS skills now</h2>
-      <h3>2. Pick up a new challenge for your portfolio and achieve more </h3>
+      <h2>Practice your JS skills now</h2>
+      <ul className="flex flex-col sm:flex-row items-center gap-3">
+        <li>Basic Challenges</li>
+        <li>Algorithm Challenges</li>
+      </ul>
+      <h3>Pick up a new challenge for your portfolio and achieve more! </h3>
     </div>
   );
 };
