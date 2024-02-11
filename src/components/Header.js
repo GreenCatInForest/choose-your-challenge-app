0a0a461978413cbeab36cd61bb8453ff004a04bc
GreenCatInForest <event2016.2017@gmail.com>
2024-02-11 18:21:08 +0000

    style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,22 @@
+import jsLogo from "../assets/images/js-logo.png";
+
 export const Header = () => {
-  return <div className="">Header</div>;
+  return (
+    <div className="flex flex-col justify-center text-center items-center">
+      <div className="flex justify-center text-center items-center">
+        <a href="/">
+          <h1 className="text-5xl sm:text-3xl font-bold mr-12 hover:transform hover:scale-150">
+            YK
+          </h1>
+        </a>
+        <div className="title flex flex-row items-center">
+          <h2>Find Your</h2>
+          <img src={jsLogo} alt="Logo JS" className="w-14" />
+          <h1>Challenge</h1>
+        </div>
+      </div>
+      <h2>1. Practice your JS skills now</h2>
+      <h3>2. Pick up a new challenge for your portfolio and achieve more </h3>
+    </div>
+  );
 };
