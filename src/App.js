9941d08345f585adc7e852a32d2d9e0bc80fee55
GreenCatInForest <event2016.2017@gmail.com>
2024-02-11 19:33:58 +0000

    add main component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { Header } from "./components/Header";
-import { Form } from "./components/Form";
-import { Challenges } from "./components/Challenges";
+import { Main } from "./components/Main";
 import { Footer } from "./components/Footer";
 
 export const App = () => {
   return (
     <div className="w-full mx-auto">
       <Header />
-      <Form />
-      <Challenges />
+      <Main />
       <Footer />
     </div>
   );
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import { Form } from "./Form";
+import { Challenges } from "./Challenges";
+
+export const Main = () => {
+  return (
+    <div>
+      <Form />
+      <Challenges />
+    </div>
+  );
+};
