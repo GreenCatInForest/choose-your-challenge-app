8cd6587c6e0d5fdbe7a8e4b375ddaa8577a7f72e
GreenCatInForest <event2016.2017@gmail.com>
2024-02-12 16:05:36 +0000

    add generate pages functionality

--- src/components/Challenges.js
+++ src/components/Challenges.js
@@ -8,6 +8,10 @@ export const Challenges = ({
   challenges,
   setChallenges,
 }) => {
+  const [numberOfChallenges, setNumberOfChallenges] = useState(0);
+  const challengesPerPage = 10;
+  const [numberOfPages, setNumberOfPages] = useState(0);
+
   useEffect(() => {
     if (searchQuery) {
       const regex = new RegExp(searchQuery, "gi");
@@ -20,21 +24,46 @@ export const Challenges = ({
         return false;
       });
       setChallenges(matchedChallenges);
+      setNumberOfChallenges(matchedChallenges.length);
     } else {
       console.log("Enter please a search query");
     }
   }, [searchQuery]);
 
   useEffect(() => {
-    if (challenges.length) console.log(challenges);
-    else console.log("There is no matches. Try some another query please");
+    if (challenges.length) {
+      console.log(challenges);
+    } else console.log("There is no matches. Try some another query please");
   }, [challenges]);
+
+  useEffect(() => {
+    if (numberOfChallenges) {
+      console.log(numberOfChallenges);
+      setNumberOfPages(numberOfChallenges / challengesPerPage);
+    } else
+      console.log(
+        "There is no number of challenges. Try some another query please"
+      );
+  }, [numberOfChallenges]);
+
+  useEffect(() => {
+    if (numberOfPages) {
+      console.log(numberOfPages);
+    } else
+      console.log("There is no number of pages. Try some another query please");
+  }, [numberOfPages]);
 
   return (
     <div className="flex-col flex-wrap text-center w-11/12 mx-auto">
       <article className="challenges flex flex-row flex-wrap justify-center gap-4 my-6">
+        {/* Render Challenges Cards  */}
         {challenges.map((challenge) => (
-          <ChallengeCard key={challenge.challengeTitle} challenge={challenge} />
+          <div key={challenge.challengeTitle}>
+            <ChallengeCard
+              key={challenge.challengeTitle}
+              challenge={challenge}
+            />
+          </div>
         ))}
       </article>
       <article>
