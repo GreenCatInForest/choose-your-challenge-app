7f6465ec43891987358a2802f8a5c97f231b051d
GreenCatInForest <event2016.2017@gmail.com>
2024-02-12 13:50:49 +0000

    clean challenges.js

--- src/components/Challenges.js
+++ src/components/Challenges.js
@@ -30,25 +30,6 @@ export const Challenges = ({
     else console.log("There is no matches. Try some another query please");
   }, [challenges]);
 
-  //   if (searchQuery) {
-  //     const matchedChallenges = datas.challenges.filter((data) => {
-  //       for (const key in data) {
-  //         if (typeof data[key] === "string" && data[key].match(regex)) {
-  //           return true;
-  //         }
-  //       }
-  //       return false;
-  //     });
-  //     console.log(matchedChallenges);
-  //     console.log(challenges);
-  //   } else {
-  //     console.log("Enter please a search query");
-  //   }
-  // }, [searchQuery]);
-
-  // searchQuery?(setChallenges(matchedChallenges)):console.log("No search query");
-  // console.log(challenges);
-
   return (
     <div className="flex-col flex-wrap text-center w-11/12 mx-auto">
       <article className="flex flex-row flex-wrap justify-center gap-4 my-6">
