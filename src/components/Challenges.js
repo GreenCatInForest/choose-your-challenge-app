b1507880d7565c33b1e45f9fa7cdf625753147c0
GreenCatInForest <event2016.2017@gmail.com>
2024-02-12 13:07:07 +0000

    add usEffect to filter challenges

--- src/components/Challenges.js
+++ src/components/Challenges.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { ChallengeCard } from "./ChallengeCard";
 import { ListChallenges } from "./ListChallenges";
 
@@ -7,20 +8,26 @@ export const Challenges = ({
   challenges,
   setChallenges,
 }) => {
-  if (searchQuery) {
+  useEffect(() => {
     const regex = new RegExp(searchQuery, "gi");
-
-    const matchedChallenges = datas.challenges.filter((data) => {
-      for (const key in data) {
-        if (typeof data[key] === "string" && data[key].match(regex)) {
-          return true;
+    if (searchQuery) {
+      const matchedChallenges = datas.challenges.filter((data) => {
+        for (const key in data) {
+          if (typeof data[key] === "string" && data[key].match(regex)) {
+            return true;
+          }
         }
-      }
-      return false;
-    });
+        return false;
+      });
+      console.log(matchedChallenges);
+      console.log(challenges);
+    } else {
+      console.log("Enter please a search query");
+    }
+  }, [searchQuery]);
 
-    console.log("Matched challenges:", matchedChallenges);
-  }
+  // searchQuery?(setChallenges(matchedChallenges)):console.log("No search query");
+  // console.log(challenges);
 
   return (
     <div className="flex-col flex-wrap text-center w-11/12 mx-auto">
