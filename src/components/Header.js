e0e0ca16994484ebdc24465967dfeb4db09d208c
GreenCatInForest <event2016.2017@gmail.com>
2024-02-13 00:44:17 +0000

    add alert

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,7 +25,7 @@ export const Header = () => {
         <button
           type="button"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-6"
-          onClick={alert("Will be implemented soon!")}
+          onClick={alert("Local Storage will be implemented soon!")}
         >
           Local Storage
         </button>
